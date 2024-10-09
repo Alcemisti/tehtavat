896208592412118a1d80c348c8dcc525a6529541
Aapo Korhonen <juupelin@proton.me>
2024-10-09 11:09:05 +0300

    Delete palautettavat tehtävät/AapoKorhonn_Tehtava7B.py

--- palautettavat tehtävät/AapoKorhonn_Tehtava7B.py
+++
@@ -1,5 +0,0 @@
-word = input("ANNA SANA! ")
-reversed_word = ""
-for käännettysana in range(len(word) - 1, -1, -1):
-    reversed_word += word[käännettysana].swapcase()
-print("Käännettynä:", reversed_word)

b5dd6e87a3357bc92b519bf28e69c550aea2d194
Aapo Korhonen <juupelin@proton.me>
2024-10-22 21:13:58 +0300

    Update AapoKorhonen_Tehtava7C.py

--- palautettavat tehtävät/AapoKorhonen_Tehtava7C.py
+++ palautettavat tehtävät/AapoKorhonen_Tehtava7C.py
@@ -1,5 +1,15 @@
-#testi
-#moika
-#miks ei toim
-#lisää teztiä
-epoirjgorekjpoejoiegjhjiiroih
+def jarjesta():
+    for i in range(0, len(list)):
+        for j in range(i+1, len(list)):
+            if list[i] >= list[j]:
+                list[i], list[j] = list[j], list[i]
+    
+    print("Numerot pienimmästä suurimpaan:", list)
+list = []
+kysy = input("Anna jokin kokonaisluku (jätä tyhjäksi lopettaaksesi): ")
+
+while kysy != "":
+    list.append(int(kysy))
+    kysy = input("Anna jokin toinen kokonaisluku (jätä tyhjäksi lopettaaksesi): ")
+    
+jarjesta()
